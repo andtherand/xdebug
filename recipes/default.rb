a172542d377063f35b656a7840ba93f3d33043f1
Andreas Ruck <mychiara@gmail.com>
2013-04-25 22:57:56 +0200

    initial commit to be able to use this one with librarian-chef

---
+++ recipes/default.rb
@@ -0,0 +1,16 @@
+include_recipe "php"
+include_recipe "apache2"
+
+#Install Xdebug
+php_pear "xdebug" do
+  action :install
+end
+
+template "#{node['php']['ext_conf_dir']}/xdebug.ini" do
+  source "xdebug.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+  notifies :restart, resources("service[apache2]"), :delayed
+end
