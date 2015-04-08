9a4120a378e278da948845a6c50ce6af23bbe76d
Andreas Ruck <mychiara@gmail.com>
2015-04-08 13:30:17 +0200

    removed case platform_family

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,9 @@
 default[:xdebug][:ini] =
-  case node['platform_family']
-    when 'debian'
-      case node['platform']
-        when 'ubuntu'
-          if node['platform_version'].to_f >= 14.04
-            '/etc/php/apache2/conf.d/20-xdebug.ini'
-          end
-      end
-    else
-      "#{node[:php][:ext_conf_dir]}/xdebug.ini"
+  case node['platform']
+    when 'ubuntu'
+      if node['platform_version'].to_f >= 14.04
+        '/etc/php/apache2/conf.d/20-xdebug.ini'
+      else
+        "#{node[:php][:ext_conf_dir]}/xdebug.ini"
+    end
   end
