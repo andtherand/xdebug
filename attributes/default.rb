04e968566832b34428cd23f98e5fb07375ce7abc
Andreas Ruck <mychiara@gmail.com>
2015-04-08 14:45:31 +0200

    removed the platform case to leave a simple default ini

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,1 @@
-default[:xdebug][:ini] =
-  case node['platform']
-    when 'ubuntu'
-      if node['platform_version'].to_f >= 14.04
-        '/etc/php5/apache2/conf.d/20-xdebug.ini'
-      else
-        "#{node[:php][:ext_conf_dir]}/xdebug.ini"
-    end
-  end
+default[:xdebug][:ini] = '/etc/php5/apache2/conf.d/20-xdebug.ini'
